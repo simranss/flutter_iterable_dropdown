b002de896b7a878741cf48c59cf84387b42e00dc
Simran Sharma <simran.s.sharma00@gmail.com>
2025-10-01 00:10:41 +0530

    Updated the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -53,37 +53,32 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            IterableDropdown<String>.builder(
-              controller: _dropdownController,
-              wrapStyle: WrapStyle.list,
-              items: [
-                IterableDropdownItem(key: 'abc', label: 'ABC', value: 'abc'),
-                IterableDropdownItem(key: 'def', label: 'DEF', value: 'def'),
-                IterableDropdownItem(key: 'ghi', label: 'GHI', value: 'ghi'),
-                IterableDropdownItem(key: 'jkl', label: 'JKL', value: 'jkl'),
-                IterableDropdownItem(key: 'mno', label: 'MNO', value: 'mno'),
-                IterableDropdownItem(key: 'pqr', label: 'PQR', value: 'pqr'),
-                IterableDropdownItem(key: 'stu', label: 'STU', value: 'stu'),
-              ],
-              itemBuilder: (_, _, item, selected, select) {
-                return ListTile(
-                  key: ValueKey(item.key),
-                  title: Text(item.label),
-                  onTap: select,
-                  trailing: Icon(
-                    selected
-                        ? Icons.check_box_rounded
-                        : Icons.check_box_outline_blank_rounded,
-                  ),
-                );
-              },
-              hintText: 'Select an option',
-              selectionMode: SelectionMode.multi,
-            ),
+        child: IterableDropdown<String>.builder(
+          controller: _dropdownController,
+          wrapStyle: WrapStyle.list,
+          items: [
+            IterableDropdownItem(key: 'abc', label: 'ABC', value: 'abc'),
+            IterableDropdownItem(key: 'def', label: 'DEF', value: 'def'),
+            IterableDropdownItem(key: 'ghi', label: 'GHI', value: 'ghi'),
+            IterableDropdownItem(key: 'jkl', label: 'JKL', value: 'jkl'),
+            IterableDropdownItem(key: 'mno', label: 'MNO', value: 'mno'),
+            IterableDropdownItem(key: 'pqr', label: 'PQR', value: 'pqr'),
+            IterableDropdownItem(key: 'stu', label: 'STU', value: 'stu'),
           ],
+          itemBuilder: (_, _, item, selected, select) {
+            return ListTile(
+              key: ValueKey(item.key),
+              title: Text(item.label),
+              onTap: select,
+              trailing: Icon(
+                selected
+                    ? Icons.check_box_rounded
+                    : Icons.check_box_outline_blank_rounded,
+              ),
+            );
+          },
+          hintText: 'Select an option',
+          selectionMode: SelectionMode.multi,
         ),
       ),
     );
